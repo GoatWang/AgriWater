0692de9ecae9ff8ce3faec7bb57a5ef2bff2f91d
JeremyWang <jeremy4555@yahoo.com.tw>
2018-12-08 16:02:04 +0800

    add static

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('get_all_coordinates/', views.get_all_coordinates, name='get_all_coordinates'),
     path('get_point_data/<int:point_number>/', views.get_point_data, name='get_point_data'),
 
-    path('election_index/', views.election_index, name='election_index'),
-    path('election_visualize/', views.election_visualize, name='election_visualize'),
-    path('election_visualize/<int:caseid>', views.election_visualize, name='election_visualize'),
+    # path('election_index/', views.election_index, name='election_index'),
+    # path('election_visualize/', views.election_visualize, name='election_visualize'),
+    # path('election_visualize/<int:caseid>', views.election_visualize, name='election_visualize'),
     # path('callback/', views.callback, name='callback'),
 ]
